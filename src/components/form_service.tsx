a2a33d5324e4d1eaa618cc7cc3ad064d4777a9ca
Armando Plácido <armando.junior4440@gmail.com>
2024-09-06 21:32:18 -0300

    Adicionando novo input

--- src/components/form_service.tsx
+++ src/components/form_service.tsx
@@ -10,13 +10,15 @@ import { Label } from "./ui/label"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 import { tipoAtendimento } from "@/type/tipo_atendimento"
 import InputError from "./ui/input_error"
-import { MaskedInput } from "./ui/input_mask";
+import { FormEvent } from "react"
+import { Textarea } from "./ui/textarea"
 
 const FormSchema = z.object({
-  prestador:z.string().min(1,"prestador é obrigatório"),
-  atendente:z.string().min(1,"atendente é obrigatório"),
-  tipo_atendimento:z.enum(tipoAtendimento,{required_error:"tipo de atendimento é obrigatório"}),
+  prestador:z.string().min(1,"Prestador inválido"),
+  atendente:z.string().min(1,"Atendente inválido"),
+  tipo_atendimento:z.enum(tipoAtendimento,{required_error:"Tipo de atendimento inválido"}),
   telefone:z.string().min(14, "Insira um número de telefone válido").max(15, "Número de telefone inválido"),
+  observacao:z.string().optional()
 })
 
 
@@ -29,6 +31,28 @@ export default function FormService() {
     console.log(data)
   }
 
+  function handlerFormatPhone(event: FormEvent<HTMLInputElement>) {
+    let value = event.currentTarget.value;
+    value = value.replace(/\D/g, "")
+
+    // Verifica a quantidade de dígitos para aplicar a máscara correta
+    if (value.length > 10) {
+      // Formato para números com 9 dígitos
+      value = value.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    } else if (value.length > 6) {
+      // Formato para números com 8 dígitos
+      value = value.replace(/^(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+    } else if (value.length > 2) {
+      // Apenas o DDD
+      value = value.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+    } else {
+      // Apenas o começo do DDD
+      value = value.replace(/^(\d{0,2})/, "($1");
+    }
+
+    event.currentTarget.value = value;
+  }
+
   return (
     <div className="self-start min-w-96 shadow-xl bg-white px-3 py-5 rounded-lg">
       <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
@@ -36,7 +60,7 @@ export default function FormService() {
         <h1 className="text-2xl">Cadastrar chamado</h1>
         <span className="h-[1px] bg-slate-300"></span>
 
-        <div className="mt-2">
+        <div className="grid w-full gap-1.5 mt-4">
           <Label htmlFor="prestador">Prestador</Label>
           <Input 
             placeholder="Nome Prestador" 
@@ -46,7 +70,7 @@ export default function FormService() {
           {errors.prestador && <InputError>{errors.prestador.message}</InputError>}
         </div>
 
-        <div className="mt-2">
+        <div className="grid w-full gap-1.5 mt-4">
           <Label htmlFor="atendente">Atendente</Label>
           <Input 
             placeholder="Nome Atendente" 
@@ -56,33 +80,20 @@ export default function FormService() {
           {errors.atendente && <InputError>{errors.atendente.message}</InputError>}
         </div>
 
-        <div className="mt-2">
+        <div className="grid w-full gap-1.5 mt-4">
           <Label htmlFor="telefone">Telefone</Label>
-          <Controller
-            name="telefone"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <MaskedInput
-                mask={"(99) 99999-9999"}
-                {...field}
-                className="border p-2"/>  
-            )}
-          />
-          {errors.telefone && <InputError>{errors.telefone.message}</InputError>}
-            
-       
-          {/* <Input 
+          <Input 
             placeholder="(00) 00000-0000" 
             type="tel" 
             inputMode="numeric"
             maxLength={15}
             id="telefone" 
-            {...register('telefone', {pattern: ^\(\d{2}\) \d \d{4}-\d{4}$  })} />
-          {errors.atendente && <InputError>{errors.atendente.message}</InputError>} */}
+            onKeyUp={handlerFormatPhone}
+            {...register('telefone')} />
+          {errors.atendente && <InputError>{errors.atendente.message}</InputError>}
         </div>
 
-        <div className="mt-2">
+        <div className="grid w-full gap-1.5 mt-4">
         <Label htmlFor="tipo_atendimento">Tipo de atendimento</Label>
         <Controller
           name="tipo_atendimento"
@@ -102,6 +113,15 @@ export default function FormService() {
             {errors.tipo_atendimento && (
             <InputError>{errors.tipo_atendimento.message}</InputError>
             )}
+        </div>
+
+        <div className="grid w-full gap-1.5 mt-4">
+          <Label htmlFor="observacao">Observação</Label>
+          <Textarea 
+            className="min-h-28"
+            id="observacao" 
+            {...register('observacao')} />
+          {errors.observacao && <InputError>{errors.observacao.message}</InputError>}
         </div>
 
         <Button className="mt-6" type="submit">Cadastrar</Button>
